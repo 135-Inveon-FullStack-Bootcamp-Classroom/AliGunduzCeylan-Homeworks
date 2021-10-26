d89848219c945002701051f6e8e2d221ee8ec13a
Aligndzcyln <ceylanali80@gmail.com>
2021-10-26 21:17:49 +0300

    import { useState, useEffect } from 'react'

--- week-2/to-do-app/src/components/index.js
+++ week-2/to-do-app/src/components/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Form from './form';
+import List from './list';
 
 function Todo() {
     const [tasks, setTasks] = useState([
@@ -17,9 +19,10 @@ function Todo() {
     ]);
 
     return (
-        <div>
-
-        </div>
+        <section className="todoapp">
+            <Form tasks={tasks} setTasks={setTasks}></Form>
+            <List tasks={tasks} setTasks={setTasks}></List >
+        </section>
     )
 }
 
---
+++ week-2/to-do-app/src/components/list/index.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react'
+
+function List({ tasks, setTasks }) {
+
+    const [copyTask, setCopyTask] = useState(tasks);
+    const [filterList, setFilterList] = useState([false, false, false, false]);
+
+    useEffect(() => {
+        setCopyTask([...tasks])
+    }, [tasks])
+
+    const destroyTask = (e) => {
+        const id = e.target.id;
+        setTasks(tasks.filter((item, index) => index != id))
+    }
+
+    const checked = (e) => {
+        const name = e.target.name;
+        setTasks(
+            tasks.map((item, index, arr) => {
+                if (item.task == name) {
+                    console.log(item.isCompleted)
+                    item.isCompleted = item.isCompleted ? false : true;
+                    return item
+                }
+                else {
+                    return item;
+                }
+            })
+        )
+
+
+
+    }
+
+    const filter = (e) => {
+        const name = e.target.name;
+
+        if (name == "all") {
+            setCopyTask([...tasks]);
+            setFilterList([true, false, false, false]);
+        }
+        else if (name == "active") {
+            setCopyTask(tasks.filter(item => item.isCompleted == false));
+            setFilterList([false, true, false, false]);
+        }
+        else if (name == "completed") {
+            setCopyTask(tasks.filter(item => item.isCompleted == true));
+            setFilterList([false, false, true, false]);
+        }
+        else if (name == "clear") {
+            setTasks(tasks.filter(item => !item.isCompleted));
+            setFilterList([false, false, false, true]);
+        }
+    }
+
+    return (
+        <div>
+            {/* <!-- This section should be hidden by default and shown when there are todos --> */}
+            <section className="main">
+                <input className="toggle-all" type="checkbox" />
+                <label htmlFor="toggle-all">Mark all as complete</label>
+
+                <ul className="todo-list">
+
+                    {copyTask.map((tasks, index) => (
+                        <li key={index} className={tasks.isCompleted ? "completed" : "view"} >
+                            <div className="view">
+                                <input name={tasks.task} style={{ width: "100%" }} className="toggle" type="checkbox" onClick={checked} />
+                                <label >{tasks.task}</label>
+                                <button id={index} className="destroy" onClick={destroyTask}></button>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul>
+            </section>
+
+            {/* <!-- This footer should hidden by default and shown when there are todos --> */}
+            <footer className="footer">
+                {/* <!-- This should be `0 items left` by default --> */}
+                <span className="todo-count">
+                    <strong>2</strong>
+                    items left
+                </span>
+
+                <ul className="filters">
+                    <li>
+                        <a name="all" onClick={filter} className={filterList[0] ? "selected" : " "} >All</a>
+                    </li>
+                    <li>
+                        <a name="active" onClick={filter} className={filterList[1] ? "selected" : " "}>Active</a>
+                    </li>
+                    <li>
+                        <a name="completed" onClick={filter} className={filterList[2] ? "selected" : " "}>Completed</a>
+                    </li>
+                </ul>
+
+                {/* <!-- Hidden if no completed items are left ↓ --> */}
+                <button name="clear" onClick={filter} className={filterList[3] ? "selected clear-completed" : "clear-completed"}>Clear completed</button>
+            </footer>
+
+
+        </div>
+    )
+}
+
+export default List;
